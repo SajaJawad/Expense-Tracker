bca8dd3dfcfac381b11a36151d442f539f327aa0
SajaJawad <saiamaher2002@gmail.com>
2025-09-08 12:09:58 +0300

    Add Income APIs

--- backend/controller/incomeController.js
+++ backend/controller/incomeController.js
@@ -2,16 +2,41 @@ const User = require("../models/User")
 const Income = require("../models/Income")
 
 //Add Income source
-exports.addIncome = async (req, res) => {}
+exports.addIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const { icon, source, amount, date } = req.body
+
+        //Validation: Check for missing fields
+        if (!source || !amount || !date) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+        const newIncome = new Income({
+            userId,
+            icon,
+            source,
+            amount,
+            date: new Date(date)
+        })
+        await newIncome.save();
+        res.status(200).json(newIncome);
+
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" })
+    }
+
+}
+
 
 
 //Get All Income 
-exports.getAllIncome = async (req, res) => {}
+exports.getAllIncome = async (req, res) => { }
 
 
 //Delete Income  
-exports.deleteIncome = async (req, res) => {}
+exports.deleteIncome = async (req, res) => { }
 
 
 //Download Income Excel 
-exports.downloadIncomeExcel = async (req, res) => {}
+exports.downloadIncomeExcel = async (req, res) => { }
