88d16845b357e5ea96e1d4469f92498cc3337a62
SajaJawad <saiamaher2002@gmail.com>
2025-09-09 11:21:49 +0300

    Get expense transaction in the last 30 days

--- backend/controller/dashboardController.js
+++ backend/controller/dashboardController.js
@@ -26,15 +26,23 @@ exports.getDashboardData = async (req, res) => {
 
         //Get income transaction in the last 60 day
         const last60DaysIncomeTransactions = await Income.find({
-            userId, 
-            date:{$gte: new Date(Date.now() - 60 * 24 * 60* 60* 1000)}
-        }).sort({date: -1})
+            userId,
+            date: { $gte: new Date(Date.now() - 60 * 24 * 60 * 60 * 1000) }
+        }).sort({ date: -1 })
 
         //Get total income for last 60 days
         const incomeLast60Days = last60DaysIncomeTransactions.reduce(
-            (sum, transaction)=>sum + transaction.amount,
+            (sum, transaction) => sum + transaction.amount,
             0
         );
+
+        //Get expense transaction in the last 30 days
+        const last30DaysExpenseTransactions = await Expense.find({
+            userId,
+            date: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) }
+        }).sort({date:-1});
+
+
 
         
     }
