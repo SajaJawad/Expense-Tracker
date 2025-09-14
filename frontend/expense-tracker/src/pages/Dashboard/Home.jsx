9f3bdb43a8d17b847a5f7aae52d26ef7b8e6dc02
SajaJawad <saiamaher2002@gmail.com>
2025-09-14 11:24:09 +0300

    InfoCard Done

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -4,6 +4,10 @@ import { useUserAuth } from '../../hooks/useUserAuth';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
+import InfoCard from '../../componants/Cards/InfoCard';
+  import {LuHandCoins, LuWalletMinimal} from 'react-icons/lu'
+  import {IoMdCard} from "react-icons/io"
+import { addThousandsSeparator } from '../../utils/helper';
 
 const Home = () => {
   useUserAuth();
@@ -12,6 +16,8 @@ const Home = () => {
 
   const [dashboardData, setDashboardData] = useState(null)
   const [loading, setLoading] = useState(false)
+
+
 
 
   const fetchDashboardData = async () => {
@@ -43,7 +49,34 @@ useEffect(()=>{
 
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div className='my-5 mx-auto'>Home</div>
+      <div className='my-5 mx-auto'>
+
+        <div className=' grid grid-cols-1 md:grid-cols-3 gap-6'>
+          <InfoCard
+          icon= {<IoMdCard/>}
+          label="Total Balance"
+          value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+          color="bg-primary"
+          />
+
+          <InfoCard
+          icon= {<LuWalletMinimal/>}
+          label="Total Income"
+          value={addThousandsSeparator(dashboardData?.totalIncome || 0)}
+          color="bg-orange-500"
+          />
+
+          <InfoCard
+          icon= {<LuHandCoins/>}
+          label="Total Expense"
+          value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
+          color="bg-red-500"
+          />
+
+
+
+        </div>
+      </div>
     </DashboardLayout>
 
   )
